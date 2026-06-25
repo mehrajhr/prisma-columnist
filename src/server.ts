7637ed9300c423f648f9c1a328ac50fdb4bc4b5a
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-06-25 23:11:26 +0600

    setting up multi prisma schema file and define user model and profile model

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import app from "./app";
 import config from "./config";
-// import { prisma } from "./lib/prisma";
+import { prisma } from "./lib/prisma";
 
 const port = config.port;
 
 async function main() {
   try {
-    // await prisma.$connect();
-    // console.log("Database connected succesfully");
+    await prisma.$connect();
+    console.log("Database connected succesfully");
     app.listen(port, () => {
       console.log(`Server running on port : ${port}`);
     });
   } catch (error) {
     console.log("Error starting the server", error);
-    // await prisma.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
