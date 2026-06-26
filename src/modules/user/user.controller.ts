5cf6fbd06713a5f7cb8b438a45a615feab2504d3
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-06-26 23:30:46 +0600

    add sendResponse in utils for reduce code of response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,19 +1,21 @@
-import type { NextFunction, Request, RequestHandler, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
+
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await userService.registerUserInDB(payload);
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
       success: true,
-      message: "User created succesfully",
-      data: {
-        user,
-      },
+      statusCode: httpStatus.CREATED,
+      message: "User registered succesfully",
+      data: { user },
     });
   },
 );
