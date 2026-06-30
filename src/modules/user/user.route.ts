e223c73176943bd783338ba93a02826de7d77d32
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-07-01 02:29:55 +0600

    cleaning and organizing the auth middleware

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,87 +1,12 @@
-import {
-  Router,
-  type NextFunction,
-  type Request,
-  type Response,
-} from "express";
+import { Router } from "express";
 import { userController } from "./user.controller";
-import { jwtUtils } from "../../utils/jwtUtils";
-import config from "../../config";
-import { Role } from "../../../generated/prisma/enums";
-import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from "http-status";
-import { catchAsync } from "../../utils/catchAsync";
-import type { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../../lib/prisma";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        email: string;
-        name: string;
-        id: string;
-        role: Role;
-      };
-    }
-  }
-}
+import { Role } from "../../../generated/prisma/enums";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
-
-const auth = (...requiredRoles: Role[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token =
-      req.cookies.accessToken ||
-      (req.headers.authorization?.startsWith("Bearer ")
-        ? req.headers.authorization?.split(" ")[1]
-        : req.headers.authorization);
-
-    if (!token) {
-      throw new Error(
-        "Your are not logged in! Please log in to access this resource.",
-      );
-    }
-
-    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
-
-    if (!verifiedToken.success) {
-      throw new Error(verifiedToken.message);
-    }
-
-    const { email, name, id, role } = verifiedToken.data as JwtPayload;
-
-    if (requiredRoles.length && !requiredRoles.includes(role)) {
-      throw new Error(
-        "Forbidden! You don't have permission to access this resource . ",
-      );
-    }
-
-    const user = await prisma.user.findUniqueOrThrow({
-      where: {
-        id,
-        email,
-        name,
-        role,
-      },
-    });
-
-    if (user.activeStatus === "BLOCKED") {
-      throw new Error("Your account has been blocked. Please contact support.");
-    }
-
-    req.user = {
-      email,
-      name,
-      id,
-      role,
-    };
-
-    next();
-  });
-};
 
 router.get(
   "/me",
