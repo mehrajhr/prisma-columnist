a5427fa90a1358554cc9ad60732a190c9a5950db
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-08-13 23:32:15 +0600

    handling prisma error with global error handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import { Prisma } from "../../generated/prisma/client";
 
 export const globalErrorHandler = (
   err: any,
@@ -8,10 +9,18 @@ export const globalErrorHandler = (
   next: NextFunction,
 ) => {
     console.log(err);
+
+    let statusCode;
+    let errorMessage = err.message || "Internal Server Error";
+
+    if(err instanceof Prisma.PrismaClientValidationError){
+          statusCode = httpStatus.BAD_REQUEST;
+          errorMessage = "You have provided incorrect field type or missing field"
+    }
   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
-    statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-    message: err.message,
+    statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+    message: errorMessage,
     error: err,
   });
 };
