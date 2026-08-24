7ea0cd6949acc843c3a800715607dd34853dd04d
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-08-24 22:04:03 +0600

    setting up stripe webhook endpoint route for suscription

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import { commentRoutes } from "./modules/comment/comment.route";
 import { notFound } from "./middlewares/notFound";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { subscriptionRoutes } from "./modules/subscription/subscripton.route";
+import { stripe } from "./lib/stripe";
 
 const app: Application = express();
 
@@ -18,6 +19,55 @@ app.use(
     origin: config.app_url,
     credentials: true,
   }),
+);
+
+const endpointSecret = config.stripe_webhook_secret;
+
+app.post(
+  "/api/subscription/webhook",
+  express.raw({ type: "application/json" }),
+  (request: Request, response: Response) => {
+    let event = request.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers["stripe-signature"]!;
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret,
+        );
+      } catch (err: any) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`,
+        );
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  },
 );
 
 app.use(express.json());
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,12 +6,13 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  app_url : process.env.APP_URL,
-  bcrypt_salt_rounds : process.env.BCRYPT_SALT_ROUNDS,
-  jwt_access_secret : process.env.JWT_ACCESS_SECRET!,
-  jwt_refresh_secret : process.env.JWT_REFRESH_SECRET!,
-  jwt_access_expires_in : process.env.JWT_ACCESS_EXPIRESIN,
-  jwt_refresh_expires_in : process.env.JWT_REFRESH_EXPIRESIN,
-  stripe_price_id : process.env.STRIPE_PRICE_ID,
-  stripe_secret_key : process.env.STRIPE_SECRET_KEY
+  app_url: process.env.APP_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRESIN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRESIN,
+  stripe_price_id: process.env.STRIPE_PRICE_ID,
+  stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+  stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
 };
